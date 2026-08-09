b158d45478039adee6e4594d6af8c65073cdd14d
DHEERENDRA <lifesstudy@gmail.com>
2026-08-09 15:48:22 +0530

    Flutter: Fix missing import for WidgetsBinding in WebRTC service

--- flutter/user/lib/services/webrtc_service.dart
+++ flutter/user/lib/services/webrtc_service.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'api_service.dart';
