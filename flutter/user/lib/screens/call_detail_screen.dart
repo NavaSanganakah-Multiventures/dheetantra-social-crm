16c47731bd57c9d4dfcd85a5c127717262b5b9ba
Dheerendra Tripathi <153520649+acharypdt@users.noreply.github.com>
2026-08-25 06:33:01 +0530

    fix(flutter): import dio for recording playback options

--- flutter/user/lib/screens/call_detail_screen.dart
+++ flutter/user/lib/screens/call_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:audioplayers/audioplayers.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
